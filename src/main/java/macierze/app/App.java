1ddf91af73ec21d7572e7d8bb2649d95e93e8cdf
pankamyk <pankamyk11@gmail.com>
2020-10-20 19:39:03 +0200

    initial commit

---
+++ src/main/java/macierze/app/App.java
@@ -0,0 +1,15 @@
+package macierze.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+   public static void main( String[] args )
+   {
+      Macierz matrix1 = new Macierz();
+      //matrix1.Print();
+      System.out.println( "Hello World!" );
+   }
+}
---
+++ src/main/java/macierze/app/Macierz.java
@@ -0,0 +1,13 @@
+package macierze.app;
+
+/**
+ * Sample matrix class
+ *
+ */
+public class Macierz
+{
+   public void Print()
+   {
+      System.out.println("[0,0],\n[0,0]");
+   }
+}
