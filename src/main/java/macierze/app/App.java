43fa8333030a6887cca414806de36d6cb1b8a6df
pankamyk <pankamyk11@gmail.com>
2020-10-22 17:45:36 +0200

    made this usable on the spot... how tf did I get 5 for this?

--- src/main/java/macierze/app/App.java
+++ src/main/java/macierze/app/App.java
@@ -1,19 +1,57 @@
 package macierze.app;
 
+import java.util.Scanner;
 
 /**
- * Hello world!
+ * Basic app representing matrix opertions.
  *
  */
 public class App 
 {
-   public static void main( String[] args )
-   {
-      Matrix matrix1 = new Matrix(new int[][] { { 1, 3 }, { 1, 0 } });
-      Matrix matrix2 = new Matrix(new int[][] { { 0, 0 }, { 7, 5 } });
-   
 
-      // DEBUGING
+   /**
+    * Matrix 1.
+    *
+    */
+   private Matrix matrix1;
+   
+   /**
+    * Matrix 2.
+    *
+    */
+   private Matrix matrix2;
+
+   /**
+    * Empty class constructor.
+    *
+    */
+   public App()
+   {
+      matrix1 = null;
+      matrix2 = null;
+   }
+
+
+   /**
+    * Method for running basic presentation.
+    *
+    */
+   public void run()
+   {
+      Scanner scanner = new Scanner(System.in);      
+      
+         System.out.println("Enter range:");
+         int range = Integer.parseInt(scanner.nextLine());
+
+         System.out.println("Enter x:");
+         int x = Integer.parseInt(scanner.nextLine());
+
+         System.out.println("Enter y:");
+         int y = Integer.parseInt(scanner.nextLine());
+
+      matrix1 = new Matrix(range, x, y);
+      matrix2 = new Matrix(range, x, y);
+
       System.out.println("M1:");
       matrix1.print();
 
@@ -28,5 +66,11 @@ public class App
 
       System.out.println("M1*M2:");
       matrix1.multiply(matrix2).print();
+   }
+
+   public static void main( String[] args )
+   {
+      App app = new App();
+      app.run();
    }
 }
